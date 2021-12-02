58dfcea8f059d86d09bd4650ee267b2ecb63c678
Yaro WebDev <yg@think.studio>
2021-12-02 19:43:29 +0200

    added flexible group

---
+++ src/Support/View/FlexibleGroup.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Whitecube\NovaFlexibleContent\Support\View;
+
+use Illuminate\Support\Arr;
+
+class FlexibleGroup
+{
+    protected string $layout;
+    protected string $key;
+    protected array $attributes;
+
+    public function __construct(string $layout, string $key, array $attributes = [])
+    {
+        $this->layout     = $layout;
+        $this->key        = $key;
+        $this->attributes = $attributes;
+    }
+
+    public function layout(): string
+    {
+        return $this->layout;
+    }
+
+    public function key(): string
+    {
+        return $this->key;
+    }
+
+    public function attribute(mixed $key = null, mixed $default = null): mixed
+    {
+        if (is_null($key)) {
+            return $this->attributes;
+        }
+
+        return Arr::get($this->attributes, $key, $default);
+    }
+
+    public function collectFlexibleAttribute(mixed $key): FlexibleGroupsCollection
+    {
+        return new FlexibleGroupsCollection(Arr::get($this->attributes, $key));
+    }
+
+
+}
---
+++ src/Support/View/FlexibleGroupsCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Whitecube\NovaFlexibleContent\Support\View;
+
+use Illuminate\Support\Collection;
+
+class FlexibleGroupsCollection extends Collection
+{
+    public function __construct($items = [])
+    {
+        parent::__construct($items);
+
+        $items = [];
+        foreach ($this->items as $item) {
+            if(
+                !empty($item['layout'])
+                && !empty($item['key'])
+                && isset($item['attributes'])
+            ) {
+                $items[] = new FlexibleGroup($item['layout'], $item['key'], is_array($item['attributes'])?$item['attributes']:[]);
+            }
+        }
+
+        $this->items = $items;
+    }
+}
